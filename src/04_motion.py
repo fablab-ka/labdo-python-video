524df50f5c24a16e9b375abae5f4395610759a73
Sebastian <viel.zu.bunt@gmail.com>
2016-09-15 23:46:12 +0200

    04_motion now just displays one bounding box for all movements

--- src/04_motion.py
+++ src/04_motion.py
@@ -21,7 +21,7 @@ while True:
   if referenceFrame is None:
     avg1 = numpy.float32(gray)
 
-  cv2.accumulateWeighted(gray, avg1, 0.1)
+  cv2.accumulateWeighted(gray, avg1, 0.02)
   referenceFrame = cv2.convertScaleAbs(avg1)
 
   frameDelta = cv2.absdiff(referenceFrame, gray)
@@ -30,13 +30,19 @@ while True:
   thresh = cv2.dilate(thresh, None, iterations=2)
   (contours, _) = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-  for c in contours:
-    if cv2.contourArea(c) < min_area:
-      continue
+  # for c in contours:
+  #   if cv2.contourArea(c) < min_area:
+  #     continue
 
-    (x, y, w, h) = cv2.boundingRect(c)
-    cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+  #   (x, y, w, h) = cv2.boundingRect(c)
 
+  #   cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+  boundingBoxes = [cv2.boundingRect(c) for c in contours if cv2.contourArea(c) < min_area]
+  boundingBoxes = [(x, y, x + w, y + h) for (x, y, w, h) in boundingBoxes]
+  if len(boundingBoxes) > 0:
+      (mx1, my1, mx2, my2) = zip(*boundingBoxes)
+      cv2.rectangle(frame, (numpy.min(mx1), numpy.min(my1)), (numpy.max(mx2), numpy.max(my2)), (0, 255, 0), 2)
 
   cv2.imshow("Labdo - Video 3", frame)
   cv2.imshow("Labdo - Video 3 - reference", referenceFrame)
